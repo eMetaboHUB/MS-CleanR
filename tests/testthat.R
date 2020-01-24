4cc1c99d5cceb02f6b0984491fe532d319a2230c
Ophélie Fraisier-Vannier <ophelie.fraisier-vannier@univ-tlse3.fr>
2020-01-24 12:26:00 +0100

    Cleaning

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(mscleanr)
-
-test_check("mscleanr")
--- tests/testthat/test-msdial_functions.R
+++
@@ -1,9 +0,0 @@
-context("MSDial functions")
-library(testthat)
-
-test_that("get_samples_info get accurate number of samples", {
-    choose_project_directory(system.file("extdata", "dirtest", package = "mscleanr"))
-    expect_equal(nrow(get_samples_info()), 8)
-    expect_equal(nrow(get_samples_info(source="pos")), 8)
-    expect_equal(nrow(get_samples_info(source="neg")), 8)
-})
